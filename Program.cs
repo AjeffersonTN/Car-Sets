60cfe8a5472bcdd4b0ff8e28f8b89db3505b0adc
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-04-08 15:31:56 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace carSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           HashSet<string> Showroom = new HashSet<string>();
+            Showroom.Add("Chevelle");
+            Showroom.Add("M6");
+            Showroom.Add("Escalade");
+            Showroom.Add("H1");
+
+            // foreach (string model in Showroom)
+            // {
+            //     Console.WriteLine(model);
+            // }
+
+            Showroom.Add("Escalade");
+
+            HashSet<string> UsedLot = new HashSet<string>();
+            Showroom.Add("Escape");
+            Showroom.Add("Silverado");
+
+            Showroom.UnionWith(UsedLot);
+
+            // foreach (string model in Showroom)
+            // {
+            //     Console.WriteLine(model);
+            // }
+
+            Showroom.Remove("H1");
+
+             foreach (string model in Showroom)
+            {
+                Console.WriteLine(model);
+            }
+        }
+    }
+}
